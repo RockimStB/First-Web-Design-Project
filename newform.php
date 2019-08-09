2bb2369cf23ddc022a19060de098ab47d745adc6
Rockim St. Bernard <rockim313@hotmail.com>
2019-08-09 00:08:10 -0500

    All imported files

---
+++ newform.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Form</title>
+</head>
+<body>
+<h1>Information<h1>
+<form method="post"action="newhandler.php" method="post">
+Name:<input type="text"name="fname"><br>
+E-mail:<input type="text"name="email"><br>
+Phone Number:<input type="text"name="phonenumber"><br>
+Comment:<input type="text"name="comment"><br>
+<input type="submit">
+</form>
+</body>
+</html> 
---
+++ newhandler.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Form</title>
+</head>
+<body>
+Welcome
+<?php echo $_POST["fname"];?>
+<br/>
+Your Email address is:
+<?php echo $_POST["email"];?>
+<br/>
+Your Phone Number is:
+<?php echo $_POST["phonenumber"];?>
+<br/>
+Your Comment is:
+<?php echo $_POST["comment"];?>
+<br/>
+
+</body>
+</html>
